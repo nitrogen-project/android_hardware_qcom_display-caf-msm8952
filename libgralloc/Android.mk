CyanogenMod/android_hardware_qcom_display

gralloc: allow forcing old pixel alignment
Since http://review.cyanogenmod.org/#/c/123901/, gralloc fallbacks
to an hard-coded pixel alignment for the NV12 and NV21 pixel formats
in case libadreno_utils doesn't export get_gpu_pixel_alignment.
However, it may happen that get_gpu_pixel_alignment is defined, but
the value it returns is not correct.

Allow targets to opt-out the use of the new adreno_utils api to
prevent alignment issues.

Change-Id: Ic8fb7880d236a96f8064d7deb8d1ef0567db39b7

libgralloc/Android.mk
@@ -46,4 +46,8 @@ LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdmemalloc\"
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
 LOCAL_SRC_FILES               := ionalloc.cpp alloc_controller.cpp
 
+ifeq ($(TARGET_USE_COMPAT_GRALLOC_ALIGN),true)
+LOCAL_CFLAGS += -DADRENO_FORCE_OLD_ALIGNMENT
+endif
+
 include $(BUILD_SHARED_LIBRARY)

nitrogen-project/android_hardware_qcom_display-caf-msm8952

libgralloc/Android.mk
@@ -49,4 +49,8 @@ LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
 LOCAL_SRC_FILES               := ionalloc.cpp alloc_controller.cpp
 LOCAL_COPY_HEADERS            := alloc_controller.h memalloc.h
 
+ifeq ($(TARGET_USE_COMPAT_GRALLOC_ALIGN),true)
+LOCAL_CFLAGS += -DDISABLE_GET_PIXEL_ALIGNMENT
+endif
+
 include $(BUILD_SHARED_LIBRARY)